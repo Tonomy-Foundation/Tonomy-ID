50b2b8d52ea8121ca9618d4404b21ca501515cb8
rebal67 <rebalree1@gmail.com>
2022-07-14 11:41:30 +0200

    add MUI

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider as PaperProvider } from 'react-native-paper';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app! </Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
