488144d8e76d22c763be2b2e832d49eff09da6c6
rebal67 <rebalalh@gmail.com>
2022-09-03 16:14:24 +0200

    added router

---
+++ src/screens/testScreen.tsx
@@ -0,0 +1,25 @@
+import TButton from '../components/Tbutton';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+export default function HomeScreen({ navigation }: { navigation: NavigationProp<any> }) {
+
+
+
+  return (
+    <View style={styles.container}>
+
+      <Text>test Page </Text>
+      <TButton onPress={() => navigation.navigate('home')}>Go to Home</TButton>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
