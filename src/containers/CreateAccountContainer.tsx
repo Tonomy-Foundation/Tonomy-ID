14c8d2f59427ffeae95fe4581292fdaedc9c6e13
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-18 08:57:23 +0200

    Fixed home screen container

--- src/containers/CreateAccountContainer.tsx
+++ src/containers/CreateAccountContainer.tsx
@@ -4,48 +4,42 @@ import { StyleSheet, Text, View } from 'react-native';
 import TButton from '../components/Tbutton';
 import TTextInput from '../components/TTextInput';
 import TPasswordInput from '../components/TPasswordInput';
-import { NavigationProp } from '@react-navigation/native';
 
 export default function CreateAccountcontainer() {
-  return (
-    <View>
-      <View style={styles.container}>
-        <View style={styles.TextInputSizing}>
-          <TTextInput label="username" />
-          <TPasswordInput label="Password" />
-          <TPasswordInput label="Confirm Password" />
-        </View>
+    return (
+        <View>
+            <View style={styles.container}>
+                <View style={styles.TextInputSizing}>
+                    <TTextInput label="username" />
+                    <TPasswordInput label="Password" />
+                    <TPasswordInput label="Confirm Password" />
+                </View>
 
-        <View style={styles.CreateAccountButtonStyle}>
-          <TButton>Create Account</TButton>
-        </View>
+                <View>
+                    <TButton>Create Account</TButton>
+                </View>
+                <View>
+                    <Text>Already have an account? Login</Text>
+                </View>
 
-        <StatusBar style="auto" />
-      </View>
-    </View>
-  );
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 220,
-    marginTop: 50
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 220,
+        marginTop: 50,
+    },
 
-  TextInputSizing: {
-    width: 340,
-    height: 50,
-  },
-  CreateAccountButtonStyle: {
-    width: 340,
-    height: 50,
-    marginTop: 250
-
-  }
+    TextInputSizing: {
+        width: 340,
+        height: 50,
+    },
 });
-
-
--- src/containers/HomeScreenContainer.tsx
+++ src/containers/HomeScreenContainer.tsx
@@ -5,24 +5,22 @@ import TButton from '../components/Tbutton';
 import { NavigationProp } from '@react-navigation/native';
 
 export default function HomeScreenContainer({ navigation }: { navigation: NavigationProp<any> }) {
-  return (
-    <View>
+    return (
+        <View>
+            <View style={HomeButtonStyle.container}>
+                <TButton onPress={() => navigation.navigate('CreateAccountScreen')}>Create Account</TButton>
+                <TButton onPress={() => navigation.navigate('#')}>Scan QR</TButton>
+            </View>
 
-    <View style={HomeButtonStyle.container}>
-    <TButton onPress={() => navigation.navigate('home')}>Go to Home</TButton>
-    <TButton onPress={() => navigation.navigate('CreateAccountScreen')}>Create Account</TButton>
-    <TButton onPress={() => navigation.navigate('#')}>Scan QR</TButton>
-    </View>
-
-    <StatusBar style="auto" />
-    </View>
-  );
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const HomeButtonStyle = StyleSheet.create({
-  container: {
-    width: 340,
-    height: 50,
-    marginTop: 250
-  },
+    container: {
+        width: 340,
+        height: 50,
+        marginTop: 250,
+    },
 });
