cf1f2ce7b6910710a29478eabcec7143796829c1
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-19 11:20:56 +0200

    fixed replace and added telos logos

--- prepare.ts
+++ prepare.ts
@@ -3,10 +3,12 @@ import settings from './src/settings';
 import app from './app.default.json';
 import myPackage from './package.json';
 
-app.expo.name = settings.config.appName;
-const slug = settings.config.appName.toLowerCase().replace(' ', '-');
-const identifier = 'foundation.tonomy.projects.' + slug.replace('-', '');
+const slug = settings.config.appName.toLowerCase().replaceAll(' ', '-');
+const identifier = 'foundation.tonomy.projects.' + slug.replaceAll('-', '');
 
+// TODO some validation of the values? e.g. check the account suffix starts with '.'
+
+app.expo.name = settings.config.appName;
 app.expo.slug = slug;
 app.expo.ios.bundleIdentifier = identifier;
 app.expo.android.package = identifier;
@@ -16,8 +18,8 @@ app.expo.android.adaptiveIcon.foregroundImage = settings.config.images.logo1024;
 app.expo.web.favicon = settings.config.images.logo48;
 app.expo.version = myPackage.version;
 
-if (settings.env === 'staging') {
-    app.expo.extra.eas.projectId = '18ee7c2e-dba4-4c3d-8bd1-712e0c19b901';
+if (settings.isProduction()) {
+    app.expo.extra.eas.projectId = settings.config.expoProjectId;
 }
 
 fs.writeFileSync('./app.json', JSON.stringify(app, null, 2));
