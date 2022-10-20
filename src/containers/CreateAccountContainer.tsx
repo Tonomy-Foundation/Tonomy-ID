39904dd7049974b15a9b8f7c6b937741b2b2a7c3
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-20 18:17:49 +0200

    Using userStore to get user and save username and password

--- src/containers/CreateAccountContainer.tsx
+++ src/containers/CreateAccountContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TButton from '../components/Tbutton';
@@ -8,8 +8,25 @@ import TLink from '../components/TA';
 import { TH1 } from '../components/THeadings';
 import settings from '../settings';
 import { NavigationProp } from '@react-navigation/native';
+import useUserStore from '../stores/userStore';
 
 export default function CreateAccountContainer({ navigation }: { navigation: NavigationProp<any> }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const user = useUserStore().user;
+
+    async function onNext() {
+        setLoading(true);
+        await user.saveUsername(username);
+        await user.savePassword(password);
+        await user.createPerson();
+        setLoading(false);
+
+        navigation.navigate('fingerprint');
+    }
+
     return (
         <View style={styles.container}>
             <View>
@@ -20,15 +37,17 @@ export default function CreateAccountContainer({ navigation }: { navigation: Nav
             </View>
             <View>
                 <View style={styles.username}>
-                    <TTextInput style={styles.usernameInput} label="Username" />
+                    <TTextInput value={username} onChange={setUsername} style={styles.usernameInput} label="Username" />
                     <Text style={styles.accountSuffix}>{settings.config.accountSuffix}</Text>
                 </View>
-                <TPasswordInput label="Password" />
+                <TPasswordInput value={password} onChange={setPassword} label="Password" />
                 <TPasswordInput label="Confirm Password" />
             </View>
 
             <View>
-                <TButton onPress={() => navigation.navigate('fingerprint')}>Next</TButton>
+                <TButton onPress={onNext} disabled={loading}>
+                    Next
+                </TButton>
             </View>
             <View>
                 <Text>
