ee0474c6f2e0dcb8ada026c2e7bb7739f8a41f8d
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-28 13:20:51 +0200

    Testing

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import MainNavigation from './src/navigation/main';
 import 'expo-dev-client';
 import theme from './src/theme';
+import { throwUnhandled } from './src/utils/errors';
+
+throwUnhandled();
 
 export default function App() {
     return (
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -36,7 +36,7 @@ export class UnexpectedApplicationError extends ApplicationError {
     }
 }
 
-export function throwExpectedError(message: string, code: string) {
+export function throwExpectedError(message: string, code?: string) {
     const error = new ExpectedApplicationError(message);
     if (code) {
         error.code = code;
@@ -44,13 +44,33 @@ export function throwExpectedError(message: string, code: string) {
     throw error;
 }
 
-export function throwUnexpectedError(message: string, code: string) {
+export function throwUnexpectedError(message: string, code?: string) {
     const error = new UnexpectedApplicationError(message);
     if (code) {
         error.code = code;
     }
     throw error;
 }
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function throwUnhandled() {
+    await sleep(1000);
+    throw new Error('im not handled!');
+}
+
+global.onunhandledrejection = function (error) {
+    // Warning: when running in "remote debug" mode (JS environment is Chrome browser),
+    // this handler is called a second time by Bluebird with a custom "dom-event".
+    // We need to filter this case out:
+    if (error instanceof Error) {
+        console.log('onunhandledrejection()', error.message);
+        // TODO log error
+        // logError(error);  // Your custom error logging/reporting code
+    }
+};
 
 /*
 List of which files have which error codes, to avoide duplicates:export function throwExpectedError(message: string, code: string) {
