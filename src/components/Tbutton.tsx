a914b270493c7645636359b36d6e0596c946f2ea
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-02 16:47:41 +0100

    Fixed issue with button disabled text color

--- src/components/Tbutton.tsx
+++ src/components/Tbutton.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
+import theme from '../theme';
 
 export type ButtonProps = React.ComponentProps<typeof Button>;
 
 export default function TButton(props: ButtonProps) {
+    const styles = StyleSheet.create({
+        buttonLabel: {
+            color: props.disabled ? 'darkgrey' : 'white',
+        },
+    });
+
     return (
-        <Button {...props} mode="contained">
+        <Button {...props} mode="contained" labelStyle={styles.buttonLabel}>
             {props.children}
         </Button>
     );
