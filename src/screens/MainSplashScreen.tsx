773e9ecfc744f4567a28505950a85bd81672f28c
riae <aymenebrahim15@gmail.com>
2022-11-09 16:58:34 +0100

    implemented main splash screen, have errors

---
+++ src/screens/MainSplashScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainSplashScreenContainer from '../containers/MainSplashContainer';
+import { NavigationProp } from '@react-navigation/native';
+
+export default function MainSplashScreen({ navigation }: { navigation: NavigationProp<any> }) {
+    return;
+    <MainSplashScreenContainer></MainSplashScreenContainer>;
+}
