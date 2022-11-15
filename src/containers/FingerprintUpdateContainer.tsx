5e2bd57170798b57476fc790896f3729abfc70f0
riae <aymenebrahim15@gmail.com>
2022-11-15 15:25:53 +0100

    Added Styling

--- src/containers/FingerprintUpdateContainer.tsx
+++ src/containers/FingerprintUpdateContainer.tsx
@@ -4,21 +4,25 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import TButton from '../components/Tbutton';
 import { TH1 } from '../components/THeadings';
 import settings from '../settings';
+import theme from '../utils/theme';
 
 export default function CreateAccountContainer() {
     return (
         <View style={styles.container}>
             <View>
-                <TH1>Would you like to add a fingerprint for added security?</TH1>
+                <TH1 style={styles.headertext}>Would you like to add a fingerprint for added security?</TH1>
             </View>
             <View>
-                <Text>This is easier than using your PIN every time</Text>
+                <Text style={styles.description}>This is easier than using your PIN every time</Text>
             </View>
-            <Image source={require('../assets/images/fingerprint.png')}></Image>
-
+            <View style={styles.imagewrapper}>
+                <Image style={styles.image} source={require('../assets/images/fingerprint.png')}></Image>
+            </View>
             <View>
-                <TButton mode="contained">Next</TButton>
-                <TButton mode="contained" color={settings.config.theme.secondaryColor}>
+                <TButton style={styles.button} mode="contained">
+                    Next
+                </TButton>
+                <TButton style={styles.button} mode="contained" color={settings.config.theme.secondaryColor}>
                     Skip
                 </TButton>
             </View>
@@ -29,6 +33,27 @@ export default function CreateAccountContainer() {
 }
 
 const styles = StyleSheet.create({
+    button: {
+        width: 90,
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
+    imagewrapper: {
+        padding: 40,
+        alignSelf: 'center',
+    },
+    description: {
+        fontSize: 18,
+        marginTop: 7,
+        textAlign: 'left',
+        color: theme.colors.disabled,
+    },
+    headertext: {
+        fontSize: 35,
+        textAlign: 'left',
+    },
     container: {
         flex: 1,
     },
