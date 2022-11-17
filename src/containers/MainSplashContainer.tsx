0afd4ed71d4780ad66930971cd9f7f2a4de0e9bb
riae <aymenebrahim15@gmail.com>
2022-11-17 15:20:52 +0100

    Implemented redirection using storage class

--- src/containers/MainSplashContainer.tsx
+++ src/containers/MainSplashContainer.tsx
@@ -3,17 +3,18 @@ import { StyleSheet, View, Text, Image, ImageSourcePropType } from 'react-native
 import theme from '../utils/theme';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import useUserStore from '../store/userStore';
+import Storage from '../utils/storage';
 
 export default function MainSplashScreenContainer({ navigation }: { navigation: NavigationProp<any> }) {
     const user = useUserStore();
     useEffect(() => {
-        setTimeout(() => {
-            user.isLoggedIn().then((res) => {
-                const page = res ? 'home' : 'securitySplash';
-                navigation.navigate(page);
-            });
-        }, 500);
-    }, []);
+        setTimeout(async () => {
+            const storage = new Storage();
+            const value = await storage.retrieve('newUser');
+            const page = value ? 'home' : 'securitySplash';
+            navigation.navigate(page);
+        }, 300);
+    });
     return (
         <View>
             <Image style={styles.mainlogo} source={require('../assets/tonomy/tonomy-logo1024.png')}></Image>
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,19 +3,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { PersistantStorage } from 'tonomy-id-sdk';
 
 export default class Storage implements PersistantStorage {
-  [x: string]: any;
+    [x: string]: any;
 
-  async retrieve(key: string): Promise<any> {
-    const data = await AsyncStorage.getItem(key);
-    return JSON.parse(data);
-  }
+    async retrieve(key: string): Promise<any> {
+        const data = await AsyncStorage.getItem(key);
+        return JSON.parse(data);
+    }
 
-  async store(key: string, value: any): Promise<void> {
-    const str = JSON.stringify(value);
-    await AsyncStorage.setItem(key, str);
-  }
+    async store(key: string, value: any): Promise<void> {
+        const str = JSON.stringify(value);
+        await AsyncStorage.setItem(key, str);
+    }
 
-  async clear(): Promise<void> {
-    await AsyncStorage.clear();
-  }
+    async clear(): Promise<void> {
+        await AsyncStorage.clear();
+    }
 }
