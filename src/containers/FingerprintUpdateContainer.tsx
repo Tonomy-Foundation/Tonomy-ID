5c26d6acd3d4d36d14506962080878d5e54395a7
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-25 16:48:32 +0100

    used LayoutComponent on Fingerprint container

--- src/containers/FingerprintUpdateContainer.tsx
+++ src/containers/FingerprintUpdateContainer.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TButton from '../components/Tbutton';
-import { TH1 } from '../components/THeadings';
-import settings from '../settings';
-import theme from '../utils/theme';
+import { TH1, TP } from '../components/THeadings';
 import FingerprintIcon from '../assets/icons/FingerprintIcon';
 import LayoutComponent from '../components/layout';
 
@@ -13,24 +11,22 @@ export default function CreateAccountContainer() {
             body={
                 <View>
                     <View>
-                        <TH1 style={styles.headertext}>Would you like to add a fingerprint for added security?</TH1>
+                        <TH1>Would you like to add a fingerprint for added security?</TH1>
                     </View>
                     <View>
-                        <Text style={styles.description}>This is easier than using your PIN every time.</Text>
+                        <TP size={1}>This is easier than using your PIN every time.</TP>
                     </View>
-                    <View style={styles.container}>
-                        <View style={styles.imagewrapper}>
-                            <FingerprintIcon style={styles.image}></FingerprintIcon>
-                        </View>
+                    <View style={styles.imageWrapper}>
+                        <FingerprintIcon style={styles.image}></FingerprintIcon>
                     </View>
                 </View>
             }
             footer={
                 <View>
-                    <TButton style={styles.button} mode="contained">
+                    <TButton style={styles.marginBottom} mode="contained">
                         Next
                     </TButton>
-                    <TButton style={styles.button} mode="contained" color={settings.config.theme.secondaryColor}>
+                    <TButton style={styles.marginBottom} mode="outlined">
                         Skip
                     </TButton>
                 </View>
@@ -40,38 +36,17 @@ export default function CreateAccountContainer() {
 }
 
 const styles = StyleSheet.create({
-    button: {
-        margin: 10,
-        alignSelf: 'center',
-        width: '90%',
-    },
     image: {
         marginTop: 50,
         alignSelf: 'center',
         width: 200,
         height: 200,
     },
-    imagewrapper: {
+    imageWrapper: {
         padding: 40,
         alignSelf: 'center',
     },
-    description: {
-        fontSize: 18,
-        marginTop: 7,
-        paddingLeft: -20,
-        textAlign: 'left',
-
-        color: theme.colors.disabled,
-    },
-    headertext: {
-        fontSize: 30,
-        marginTop: 40,
-        textAlign: 'left',
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        fontWeight: '800',
-    },
-    container: {
-        flex: 1,
+    marginBottom: {
+        marginBottom: 16,
     },
 });
