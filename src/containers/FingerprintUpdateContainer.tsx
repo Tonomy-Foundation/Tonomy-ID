115b6fef01efb9c633c5c709501359935cb220e3
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-09 14:52:35 +0100

    Applied error logic to fingerprint

--- src/containers/FingerprintUpdateContainer.tsx
+++ src/containers/FingerprintUpdateContainer.tsx
@@ -9,11 +9,15 @@ import { useNavigation } from '@react-navigation/native';
 import { commonStyles } from '../utils/theme';
 import * as LocalAuthentication from 'expo-local-authentication';
 import TModal from '../components/TModal';
+import useErrorStore from '../store/errorStore';
 
 export default function CreateAccountContainer({ password }: { password: string }) {
     const [showModal, setShowModal] = useState(false);
     const user = useUserStore((state) => state.user);
+    const errorStore = useErrorStore();
+
     const navigation = useNavigation();
+
     const onNext = async () => {
         try {
             const authenticated = LocalAuthentication.isEnrolledAsync();
@@ -25,7 +29,7 @@ export default function CreateAccountContainer({ password }: { password: string 
 
             await updateKeys();
         } catch (e) {
-            //TODO: handle error with generic error component
+            errorStore.setError({ error: e, expected: false });
         }
     };
 
