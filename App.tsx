a4d1e734d87d241160fe34fcf9d185c83691c97c
rebal67 <rebalalh@gmail.com>
2022-12-10 14:29:03 +0100

    added linking

--- App.tsx
+++ App.tsx
@@ -10,10 +10,18 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import setErrorHandlers from './src/utils/exceptions';
 import ErrorHandlerContainer from './src/components/ErrorHandlerProvider';
 import useErrorStore from './src/store/errorStore';
+import * as Linking from 'expo-linking';
 
 export default function App() {
     const errorStore = useErrorStore();
+    const url = Linking.useURL();
 
+    if (url) {
+        const { hostname, path, queryParams } = Linking.parse(url);
+        console.log('hostname', hostname);
+        console.log('path', path);
+        console.log('queryParams', queryParams);
+    }
     setErrorHandlers(errorStore);
 
     return (
