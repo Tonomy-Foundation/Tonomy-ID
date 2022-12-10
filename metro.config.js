47d355ac53fc2f63abc81bc838e17c375237cf35
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-10 15:21:31 +0100

    remove console.log

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-console.log('__dirname', __dirname);
 
 // needed to resolve the did-jwt index.cjs file that is withing Tonomy-ID-SDK
 config.resolver.sourceExts.push('cjs');
