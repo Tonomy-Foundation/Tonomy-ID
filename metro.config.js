bea449f245f061f228ba687da94bbb5d7a06ab0c
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-10 14:15:38 +0100

    Cleaned up metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -9,22 +9,10 @@ config.resolver.sourceExts.push('cjs');
 
 if (process.env.NODE_ENV === 'local') {
     console.log('Setting up local development environment. Using local Tonomy-ID-SDK.');
+    // see https://medium.com/@alielmajdaoui/linking-local-packages-in-react-native-the-right-way-2ac6587dcfa2
 
     const sdkPath = __dirname + '/../Tonomy-ID-SDK';
     config.resolver.nodeModulesPath = [sdkPath];
-    // config.resolver.resolveRequest = (context, moduleName, platform) => {
-    //     if (moduleName === 'tonomy-id-sdk' || moduleName.startsWith('tonomy-id-sdk')) {
-    //         // Logic to resolve the module name to a file path...
-    //         // NOTE: Throw an error if there is no resolution.
-    //         return {
-    //             filePath: sdkPath + '/dist/index.js',
-    //             type: 'sourceFile',
-    //         };
-    //     }
-
-    //     // Optionally, chain to the standard Metro resolver.
-    //     return context.resolveRequest(context, moduleName, platform);
-    // };
     config.watchFolders.push(sdkPath);
 }
 
