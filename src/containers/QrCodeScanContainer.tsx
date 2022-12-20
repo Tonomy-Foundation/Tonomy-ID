768bef4d46c6535046db8fa3de4f48548ce2d280
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-20 09:45:10 +0100

    Fixed comments

--- src/containers/QrCodeScanContainer.tsx
+++ src/containers/QrCodeScanContainer.tsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native';
-import { TButtonContained, TButtonOutlined } from '../components/atoms/Tbutton';
-import { TH1, TP } from '../components/atoms/THeadings';
-import FingerprintIcon from '../assets/icons/FingerprintIcon';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { TButtonOutlined } from '../components/atoms/Tbutton';
+import { TP } from '../components/atoms/THeadings';
 import LayoutComponent from '../components/layout';
 import { commonStyles } from '../utils/theme';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 
 export const width = Dimensions.get('window').width * 0.01;
 export const height = Dimensions.get('window').height * 0.01;
 
 export default function QrCodeScanContainer() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasPermission, setHasPermission] = useState(null as null | boolean);
     const [scanned, setScanned] = useState(false);
 
     useEffect(() => {
@@ -23,50 +22,56 @@ export default function QrCodeScanContainer() {
         getBarCodeScannerPermissions();
     }, []);
 
-    const handleBarCodeScanned = ({ type, data }) => {
+    const handleBarCodeScanned = ({ type, data }: BarCodeScannerResult) => {
         setScanned(true);
         alert(`Bar code with type ${type} and data ${data} has been scanned!`);
     };
 
-    //   if (hasPermission === null) {
-    //     return <Text>Requesting for camera permission</Text>;
-    //   }
-    //   if (hasPermission === false) {
-    //     return <Text>No access to camera</Text>;
-    //   }
-
     return (
         <LayoutComponent
-            body={<View>
-                <View style={styles.container}>
-                    <BarCodeScanner
-                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            body={
+                <View>
+                    <View style={styles.container}>
+                        {hasPermission === null && <TP size={2}>Requesting for camera permission</TP>}
+                        {hasPermission === false && <TP size={3}>No access to camera</TP>}
+                        {hasPermission === true && (
+                            <BarCodeScanner
+                                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                                style={{
+                                    width: width * 90,
+                                    height: height * 80,
+                                }}
+                            />
+                        )}
+                    </View>
+                    <View
                         style={{
-                            width: width * 90,
-                            height: height * 80,
+                            position: 'absolute',
+                            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                            height: '100%',
+                            width: '100%',
+                            alignItems: 'center',
+                            justifyContent: 'center',
                         }}
-                    />
-
+                    >
+                        <TP
+                            size={2}
+                            style={{
+                                color: 'white',
+                            }}
+                        >
+                            Align QR Code within frame to scan
+                        </TP>
+                        <Image
+                            source={require('../assets/images/qrScannerBorders.png')}
+                            style={{
+                                height: '75%',
+                                width: '75%',
+                                resizeMode: 'contain',
+                            }}
+                        />
+                    </View>
                 </View>
-                <View style={{
-                    position: 'absolute',
-                    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                    height: '100%',
-                    width: '100%',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                    <TP style={{
-                        color: 'rgb(255, 255, 255)'
-                    }}>Align QR Code within frame to scan</TP>
-                    <Image source={require('../assets/images/qrScannerBorders.png')}
-                        style={{
-                            height: '75%',
-                            width: '75%',
-                            resizeMode: 'contain'
-                        }} />
-                </View>
-            </View>
             }
             footer={
                 <View>
@@ -91,11 +96,6 @@ const styles = StyleSheet.create({
     container: {
         height: '100%',
         width: '100%',
-        overflow:'hidden'
-        // backgroundColor: 'blue',
-        // height: 600,
-        // width: '100%',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    }
+        overflow: 'hidden',
+    },
 });
