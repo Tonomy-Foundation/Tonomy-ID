376b27a0fe06e0b802673b315857d0cbd4dcd955
StefanoPitto <stefapitto@hotmail.com.ar>
2023-01-09 14:08:40 -0300

    outline fixed on password input

--- src/components/molecules/TPasswordInput.tsx
+++ src/components/molecules/TPasswordInput.tsx
@@ -4,14 +4,20 @@ import TTextInput, { TTextInputProps } from './TTextInput';
 import theme from '../../utils/theme';
 
 export default function TPasswordInput(props: TTextInputProps) {
-    return <TTextInput underlineColor="transparent" {...props} secureTextEntry={true} style={styles.input} />;
+    return (
+        <TTextInput
+            underlineColor="transparent"
+            mode="outlined"
+            {...props}
+            secureTextEntry={true}
+            style={styles.input}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
     input: {
         backgroundColor: 'transparent',
-        borderWidth: 1,
-        borderColor: theme.colors.primary,
         marginBottom: 4,
         borderRadius: 6,
     },
