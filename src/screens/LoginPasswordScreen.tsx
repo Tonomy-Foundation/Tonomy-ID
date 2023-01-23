7c1e103e4eb2a76a93130e537a2043a4a737dd27
StefanoPitto <stefapitto@hotmail.com.ar>
2023-01-23 15:52:16 -0300

    params fixed

--- src/screens/LoginPasswordScreen.tsx
+++ src/screens/LoginPasswordScreen.tsx
@@ -4,6 +4,7 @@ import LoginPasswordContainer from '../containers/LoginPasswordContainer';
 import { RouteStackParamList } from '../navigation/Root';
 export type Props = NativeStackScreenProps<RouteStackParamList, 'LoginPassword'>;
 export default function LoginPasswordScreen(props: Props) {
+    console.log('Parametros', props);
     return (
         <LoginPasswordContainer
             username={props.route.params.username}
