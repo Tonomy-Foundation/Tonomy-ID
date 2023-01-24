a7590b30a91b3110fd6efa432d33fb1ab20b314a
alikarani <aliasgharkarani125@hotmail.com>
2023-01-24 12:51:37 +0500

    wip

--- src/containers/SettingsScreenContainer.tsx
+++ src/containers/SettingsScreenContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Image, Dimensions } from 'react-native';
 import { TButtonOutlined } from '../components/atoms/Tbutton';
-import { TP } from '../components/atoms/THeadings';
+import { TP, TCaption, TH2, TH1 } from '../components/atoms/THeadings';
 import LayoutComponent from '../components/layout';
 import { commonStyles } from '../utils/theme';
 
@@ -14,35 +14,46 @@ export default function SettingsScreenContainer() {
         <LayoutComponent
             body={
                 <View>
-                    <View style={styles.container}>
-                        {<TP size={2}>SettingsScreenContainer</TP>}
-                    </View>
                     <View
                         style={{
-                            position: 'absolute',
-                            backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                            height: '100%',
-                            width: '100%',
-                            alignItems: 'center',
-                            justifyContent: 'center',
+                            width: width * 90,
+                            alignSelf: 'center',
                         }}
                     >
-                        <TP
-                            size={2}
-                            style={{
-                                color: 'white',
-                            }}
-                        >
-                            Align QR Code within frame to scan
-                        </TP>
+                        {<TP size={1}>Security</TP>}
+                        <View style={{
+                            flexDirection: 'row'
+                        }}>
+                            <TP style={{
+                                width: '10%',
+                                marginRight: '2.5%',
+                                backgroundColor: 'purple'
+                            }}>
+                                ICON
+                            </TP>
+                            <TP size={1} style={{
+                                color: '#272727',
+                                width: '72.5%',
+                                marginRight: '2.5%',
+                                backgroundColor: 'red'
+                            }}>
+                                Password
+                            </TP>
+                            <TP style={{
+                                width: '12.5%',
+                                backgroundColor: 'yellow'
+                            }}>
+                                Arrow Icon
+                            </TP>
+                        </View>
                     </View>
                 </View>
             }
-            footer={
-                <View>
-                    <TButtonOutlined style={commonStyles.marginBottom}>Cancel</TButtonOutlined>
-                </View>
-            }
+        // footer={
+        //     <View>
+        //         <TButtonOutlined style={commonStyles.marginBottom}>Cancel</TButtonOutlined>
+        //     </View>
+        // }
         />
     );
 }
