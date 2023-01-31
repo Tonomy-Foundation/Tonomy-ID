27063aceeba3030a527eda38c2fe75ddac01218c
StefanoPitto <stefapitto@hotmail.com.ar>
2023-01-31 15:07:58 -0300

    Icons removed and updated and screen fixed.

--- src/containers/SettingsContainer.tsx
+++ src/containers/SettingsContainer.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { TouchableOpacity, View, StyleSheet, ScrollView } from 'react-native';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
 import { TCaption, TP } from '../components/atoms/THeadings';
 import LayoutComponent from '../components/layout';
-import PasswordIcon from '../assets/icons/PasswordIcon';
-import PinIcon from '../assets/icons/PinIcon';
-import SecurityQuestionsIcon from '../assets/icons/SecurityQuestionsIcon';
-import BuddiesIcon from '../assets/icons/BuddiesIcon';
-import CardIcon from '../assets/icons/CardIcon';
-import LanguageIcon from '../assets/icons/LanguageIcon';
-import LogoutIcon from '../assets/icons/LogoutIcon';
-import ChevronRightIcon from '../assets/icons/ChevronRightIcon';
 import { commonStyles } from '../utils/theme';
-import PrintIcon from '../assets/icons/PrintIcon';
-import { height } from './QrCodeScanContainer';
+
+import { IconButton } from 'react-native-paper';
 export default function SettingsContainer() {
     return (
         <LayoutComponent
@@ -21,30 +13,30 @@ export default function SettingsContainer() {
                     <TP size={1}>SECURITY</TP>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
                         <View style={styles.innerContainer}>
-                            <PasswordIcon style={styles.marginStyle} />
+                            <IconButton icon={'cog'} />
                             <TP>Password</TP>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
                         <View style={styles.innerContainer}>
-                            <PinIcon style={styles.marginStyle} />
+                            <IconButton icon={'lock'} />
                             <TP>PIN</TP>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
                         <View style={styles.innerContainer}>
-                            <PrintIcon style={styles.marginStyle} />
+                            <IconButton icon={'fingerprint'} />
                             <TP>Fingerprint</TP>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TP size={1}>AUTONOMOUS ACCOUNT RECOVERY</TP>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <SecurityQuestionsIcon style={[styles.iconStyle, styles.marginStyle]} />
-                            <View style={styles.textContainer}>
+                        <View style={styles.textContainer}>
+                            <IconButton icon={'help-circle'} />
+                            <View>
                                 <TP>Security Questions</TP>
                                 <TCaption>
                                     Allow your account to be autonomously recovered when you correctly answer questions
@@ -52,24 +44,25 @@ export default function SettingsContainer() {
                                 </TCaption>
                             </View>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <BuddiesIcon style={[styles.iconStyle, styles.marginStyle]} />
-                            <View style={styles.textContainer}>
+                        <View style={styles.textContainer}>
+                            <IconButton icon={'account-supervisor'} />
+                            <View>
                                 <TP>Recovery Buddies</TP>
                                 <TCaption>
                                     Allow your account to be autonomously recovered by a group of people you trust.
                                 </TCaption>
                             </View>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <CardIcon style={[styles.iconStyle, styles.marginStyle]} />
-                            <View style={styles.textContainer}>
+                        <View style={styles.textContainer}>
+                            <IconButton icon={'credit-card-outline'} />
+                            <View>
                                 <TP>NFC Card Recovery</TP>
                                 <TCaption>
                                     Allow your account to be autonomously recovered when you tap your secure NFC enable
@@ -77,23 +70,23 @@ export default function SettingsContainer() {
                                 </TCaption>
                             </View>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TP size={1}>LANGUAGE</TP>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
                         <View style={styles.innerContainer}>
-                            <LanguageIcon style={styles.marginStyle} />
+                            <IconButton icon={'translate'} />
                             <TP>Choose Language</TP>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                     <TP size={1}>ACCOUNT</TP>
                     <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
                         <View style={styles.innerContainer}>
-                            <LogoutIcon style={styles.marginStyle} />
+                            <IconButton icon={'logout-variant'} />
                             <TP>Logout</TP>
                         </View>
-                        <ChevronRightIcon style={styles.chevronStyle} />
+                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
                     </TouchableOpacity>
                 </View>
             }
@@ -106,25 +99,20 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 10,
-    },
-    innerContainer: {
-        flexDirection: 'row',
-    },
-    textContainer: {
-        maxWidth: '90%',
-    },
-    iconStyle: {
-        marginTop: 6,
-    },
-    marginStyle: {
-        marginRight: 6,
-    },
-    contentContainer: {
-        display: 'flex',
-        flexDirection: 'row',
+        alignItems: 'center',
     },
     chevronStyle: {
         alignSelf: 'center',
+        marginLeft: 20,
+    },
+    innerContainer: {
+        maxWidth: '85%',
+        flexDirection: 'row',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    textContainer: {
+        flexDirection: 'row',
+        width: '75%',
     },
 });
