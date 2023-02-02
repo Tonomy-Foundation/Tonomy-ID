4353cf668acf164e92567a1720aeec0b413e16d6
StefanoPitto <stefapitto@hotmail.com.ar>
2023-02-02 16:03:49 -0300

    New component added and screen modified

--- src/containers/SettingsContainer.tsx
+++ src/containers/SettingsContainer.tsx
@@ -5,114 +5,81 @@ import LayoutComponent from '../components/layout';
 import { commonStyles } from '../utils/theme';
 
 import { IconButton } from 'react-native-paper';
+import TNavigationButton from '../components/TNavigationButton';
 export default function SettingsContainer() {
     return (
         <LayoutComponent
             body={
                 <View style={{}}>
                     <TP size={1}>SECURITY</TP>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <IconButton icon={'cog'} />
-                            <TP>Password</TP>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <IconButton icon={'lock'} />
-                            <TP>PIN</TP>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <IconButton icon={'fingerprint'} />
-                            <TP>Fingerprint</TP>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Password'}
+                        icon={'cog'}
+                    />
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'PIN'}
+                        icon={'lock'}
+                    />
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Fingerprint'}
+                        icon={'fingerprint'}
+                    />
                     <TP size={1}>AUTONOMOUS ACCOUNT RECOVERY</TP>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.textContainer}>
-                            <IconButton icon={'help-circle'} />
-                            <View>
-                                <TP>Security Questions</TP>
-                                <TCaption>
-                                    Allow your account to be autonomously recovered when you correctly answer questions
-                                    only you know about your life.
-                                </TCaption>
-                            </View>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.textContainer}>
-                            <IconButton icon={'account-supervisor'} />
-                            <View>
-                                <TP>Recovery Buddies</TP>
-                                <TCaption>
-                                    Allow your account to be autonomously recovered by a group of people you trust.
-                                </TCaption>
-                            </View>
-                        </View>
-
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.textContainer}>
-                            <IconButton icon={'credit-card-outline'} />
-                            <View>
-                                <TP>NFC Card Recovery</TP>
-                                <TCaption>
-                                    Allow your account to be autonomously recovered when you tap your secure NFC enable
-                                    keycard to your phone.
-                                </TCaption>
-                            </View>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Security Questions'}
+                        icon={'help-circle'}
+                        description={
+                            'Allow your account to be autonomously recovered when you correctly answer questions only you know about your life'
+                        }
+                    />
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'NFC Card Recovery'}
+                        icon={'credit-card-outline'}
+                        description={
+                            'Allow your account to be autonomously recovered when you tap your secure NFC enabled keycard to your phone.'
+                        }
+                    />
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Recovery Buddies'}
+                        icon={'account-supervisor'}
+                        description={'Allow your account to be autonomously recovered by a group of people you trust.'}
+                    />
                     <TP size={1}>LANGUAGE</TP>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <IconButton icon={'translate'} />
-                            <TP>Choose Language</TP>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Choose Language'}
+                        icon={'translate'}
+                    />
                     <TP size={1}>ACCOUNT</TP>
-                    <TouchableOpacity style={[styles.button, commonStyles.marginBottom]}>
-                        <View style={styles.innerContainer}>
-                            <IconButton icon={'logout-variant'} />
-                            <TP>Logout</TP>
-                        </View>
-                        <IconButton icon={'chevron-right'} style={styles.chevronStyle} />
-                    </TouchableOpacity>
+                    <TNavigationButton
+                        onPress={function (): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        title={'Logout'}
+                        icon={'logout-variant'}
+                    />
                 </View>
             }
         ></LayoutComponent>
     );
 }
-
-const styles = StyleSheet.create({
-    button: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    chevronStyle: {
-        alignSelf: 'center',
-        marginLeft: 20,
-    },
-    innerContainer: {
-        maxWidth: '85%',
-        flexDirection: 'row',
-        display: 'flex',
-        alignItems: 'center',
-    },
-    textContainer: {
-        flexDirection: 'row',
-        width: '75%',
-    },
-});
