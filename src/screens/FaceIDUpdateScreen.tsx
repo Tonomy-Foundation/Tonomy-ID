d90fa3b3a3ea279a645650fdc71f3931ace990a9
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-02-28 15:51:40 +0500

    face screen removed and all logic in one screen completed

--- src/screens/FaceIDUpdateScreen.tsx
+++
@@ -1,8 +0,0 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
-import { RouteStackParamList } from '../navigation/Root';
-import FaceIdUpdateContainer from '../containers/FaceIDUpdateContainer';
-
-export default function FaceIdUpdateScreen(props: Props) {
-    return <FaceIdUpdateContainer password={props.route.params.password}></FaceIdUpdateContainer>;
-}
