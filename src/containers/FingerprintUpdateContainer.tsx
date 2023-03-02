8c555fbc6faa555fee5c7bfd93e13ebd42d502ed
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-03-02 18:15:27 +0500

    eslint fixed for fingerPrintContainer

--- src/containers/FingerprintUpdateContainer.tsx
+++ src/containers/FingerprintUpdateContainer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import { Linking, Platform, StyleSheet, View } from 'react-native';
 import { TButtonContained, TButtonOutlined } from '../components/atoms/Tbutton';
