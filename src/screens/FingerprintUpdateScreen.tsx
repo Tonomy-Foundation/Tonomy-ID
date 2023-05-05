2f80ac5a7c4569783bd27d437ab95edd13ab6044
Jack Tanner <jack@jackandtheblockstalk.com>
2023-05-05 16:39:41 +0200

    fix: fixed icon type errors

--- src/screens/FingerprintUpdateScreen.tsx
+++ src/screens/FingerprintUpdateScreen.tsx
@@ -1,7 +1,5 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import FingerprintUpdateContainer from '../containers/FingerprintUpdateContainer';
-import { RouteStackParamList } from '../navigation/Root';
 
 export default function FingerprintUpdateScreen(props: Props) {
     return <FingerprintUpdateContainer password={props.route.params.password}></FingerprintUpdateContainer>;
