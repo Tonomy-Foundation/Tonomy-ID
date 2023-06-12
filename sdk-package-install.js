ef95047aa3c8efb5200f1b20be5a953543977230
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-13 00:46:12 +0500

    update install script

--- sdk-package-install.js
+++ sdk-package-install.js
@@ -1,11 +1,17 @@
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
-const currentBranch = execSync('git symbolic-ref --short HEAD', {
-    encoding: 'utf8',
-}).trim();
+try {
+    const currentBranch = execSync('git symbolic-ref --short HEAD', {
+        encoding: 'utf8',
+    }).trim();
 
-if (currentBranch === 'feature/232-sdk-deployment') {
-    execSync('npm install --no-lockfile @tonomy/tonomy-id-test@development', {
-        stdio: 'inherit',
-    });
+    if (currentBranch === 'feature/232-sdk-deployment') {
+        execSync('yarn add @tonomy/tonomy-id-test@development', {
+            stdio: 'inherit',
+        });
+    } else {
+        console.log('Current branch is not feature/232-sdk-deployment.');
+    }
+} catch (error) {
+    console.error('An error occurred while executing Git command:', error);
 }
