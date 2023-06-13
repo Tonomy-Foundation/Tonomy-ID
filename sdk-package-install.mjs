653776aadce64c5603e6e9d43c775146917d3b28
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-13 17:21:15 +0500

    fix extension

---
+++ sdk-package-install.mjs
@@ -0,0 +1,11 @@
+import { execSync } from 'child_process';
+
+const currentBranch = execSync('git symbolic-ref --short HEAD', {
+    encoding: 'utf8',
+}).trim();
+
+if (currentBranch === 'feature/232-sdk-deployment') {
+    execSync('npm install --no-lockfile @tonomy/tonomy-id-test@development', {
+        stdio: 'inherit',
+    });
+}
