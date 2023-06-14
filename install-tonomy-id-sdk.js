9bf43e747b42615757f49078ede5549ee3e2b17c
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-15 00:40:09 +0500

    Added logs

--- install-tonomy-id-sdk.js
+++ install-tonomy-id-sdk.js
@@ -1,15 +1,20 @@
 const { execSync } = require('child_process');
 
 try {
-    const currentBranch = execSync('git rev-parse --abbrev-ref HEAD', {
+    const currentBranch = execSync('git symbolic-ref --short HEAD', {
         encoding: 'utf8',
     }).trim();
 
+    console.log(currentBranch);
+
     if (currentBranch !== 'master') {
-        execSync('npm install @tonomy/tonomy-id-sdk@development --no-lockfile', {
+        console.log('execution start');
+        execSync('npm install @tonomy/tonomy-id-sdk@development', {
             stdio: 'inherit',
         });
+        console.log('execution end');
     }
 } catch (error) {
-    console.error('An error occurred while retrieving the current branch::', error);
+    console.error('An error occurred:', error);
+    process.exit(1); // Exit the script with a non-zero code to indicate failure
 }
