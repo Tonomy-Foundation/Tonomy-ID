57fe65c64c97d0fc36cab6b9aa3658792d1176c8
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-14 15:38:54 +0500

    added try catch

--- sdk-package-install.js
+++ sdk-package-install.js
@@ -1,11 +1,15 @@
 const { execSync } = require('child_process');
 
-const currentBranch = execSync('git symbolic-ref --short HEAD', {
-    encoding: 'utf8',
-}).trim();
+try {
+    const currentBranch = execSync('git rev-parse --abbrev-ref HEAD', {
+        encoding: 'utf8',
+    }).trim();
 
-if (currentBranch === 'development') {
-    execSync('npm install @tonomy/tonomy-id-sdk@development --no-lockfile', {
-        stdio: 'inherit',
-    });
+    if (currentBranch === 'development') {
+        execSync('npm install @tonomy/tonomy-id-sdk@development --no-lockfile', {
+            stdio: 'inherit',
+        });
+    }
+} catch (error) {
+    console.error('An error occurred while retrieving the current branch:', error);
 }
