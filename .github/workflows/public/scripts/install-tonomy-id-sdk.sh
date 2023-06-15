711e4b880bd633fc80efdb83fc341305a9c6e220
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-15 19:47:36 +0500

    remove script and add in actions

--- .github/workflows/public/scripts/install-tonomy-id-sdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-currentBranch=$( github.head_ref )
-echo "current branch $currentBranch"
-if [[ "$currentBranch" != "master" ]]; then
-  echo "execution start"
-  npm install @tonomy/tonomy-id-sdk@development
-  echo "execution end"
-fi
