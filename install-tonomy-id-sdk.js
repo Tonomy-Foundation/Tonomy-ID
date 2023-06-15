17b81ec049e86045c19f6ca6a78a79426dfc5e31
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-15 14:06:50 +0500

    update current branch script

--- install-tonomy-id-sdk.js
+++ install-tonomy-id-sdk.js
@@ -3,19 +3,17 @@ const { execSync } = require('child_process');
 try {
     let currentBranch;
 
-    try {
-        currentBranch = execSync('git symbolic-ref --short HEAD', {
-            encoding: 'utf8',
-            stdio: ['pipe', 'pipe', 'ignore'], // Redirect stdout and stderr to pipes
-        }).trim();
-        console.log('current branch', currentBranch);
-    } catch (error) {
-        // Fallback command for non-symbolic reference
-        currentBranch = execSync('git branch --show-current', {
-            encoding: 'utf8',
-        }).trim();
-        console.log('current branch', currentBranch);
+    currentBranch = execSync('git symbolic-ref --short HEAD', {
+        encoding: 'utf8',
+        stdio: ['pipe', 'pipe', 'ignore'], // Redirect stdout and stderr to pipes
+    }).trim();
+    console.log('current branch', currentBranch);
+
+    if (currentBranch === '' || !currentBranch) {
+        currentBranch = process.env.GITHUB_REF.match(/refs\/heads\/(.*)/)[1];
     }
+
+    console.log('current branch', currentBranch);
 
     if (currentBranch !== 'master') {
         console.log('execution start');
