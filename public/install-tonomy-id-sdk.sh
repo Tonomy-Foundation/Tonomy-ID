f327b1278b0d4e33a977775c2e678a5fcdd23e3a
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-15 19:07:05 +0500

    added more cosnole

--- public/install-tonomy-id-sdk.sh
+++ public/install-tonomy-id-sdk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # currentBranch=$(git symbolic-ref --short HEAD)
+refBranch=$(git symbolic-ref -q HEAD)
+    echo "Checkout Branch [${refBranch:11}]"
 if currentBranch=$(git symbolic-ref -q HEAD); then
     currentBranch=${headRef#refs/heads/}
 else
