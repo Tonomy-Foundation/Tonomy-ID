c6c0415376804d3da72a5fa71dd044b6b4303bf4
sadiabbasi <38488513+sadiabbasi@users.noreply.github.com>
2023-08-08 22:40:52 +0500

    Feature/802 generate apk (#804)
    * working in github pipeline
    
    * update pipeline
    
    * tyring to fix the pipeline error
    
    * remove projectID from prepare.config
    
    * change projectid to Project ID
    
    * change path
    
    * update yaml to test android only
    
    * update project id
    
    * update build command
    
    * change projectid to Project ID
    
    * update config
    
    * change project id
    
    * change project id
    
    * update build
    
    * update build
    
    * push all
    
    * push
    
    * remove eas-cli from package
    
    * change staging
    
    * push all
    
    * push all
    
    * add
    
    * update yaml
    
    * add setup eas
    
    * change expo version
    
    * add secreT
    
    * revert changes
    
    * revert changes
    
    * update build file
    
    * uncomment android archieve
    
    * console workspace
    
    * fix: fixed for local `act` build
    
    * ad echo to see workspace
    
    * ad echo to see workspace
    
    * change projectID and dev to demo
    
    * go to previous working yaml
    
    * change development to demo
    
    * revert changes
    
    * find and rename file
    
    * change filename
    
    * change upload path
    
    * update rename apk file
    
    * fix rename file
    
    * remove rename action andupload
    
    * change development to demo
    
    * change the publish name
    
    * change the publish name
    
    * revert the build command
    
    * change demo to staging
    
    * update env
    
    * change runner to ubuntu-latest
    
    * change demo to development
    
    * change to ubuntu 20
    
    * update env
    
    * add dev project id
    
    * change to ubunut-latest
    
    * fix comments
    
    * update project id
    
    * space issue
    
    * fix find command
    
    * adding act for local testing
    
    ---------
    
    Co-authored-by: Jack Tanner <jack@jackandtheblockstalk.com>

--- prepare.app.config.ts
+++ prepare.app.config.ts
@@ -76,7 +76,7 @@ const expo: ExpoConfig = {
     ],
     extra: {
         eas: {
-            projectId: '48982c5e-41ff-4858-80ea-8e1b64092f21',
+            projectId: '3a65b31c-572d-4537-9ead-f7c4bddf7d29',
         },
         EXPO_NODE_ENV: process.env.EXPO_NODE_ENV,
     },
