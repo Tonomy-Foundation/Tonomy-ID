45951f96a6c0117929243a45aa8d2d236c24067e
sadiabbasi <sadiaabbas326@gmail.com>
2023-08-08 18:00:15 +0500

    fix comments

--- prepare.app.config.ts
+++ prepare.app.config.ts
@@ -76,7 +76,7 @@ const expo: ExpoConfig = {
     ],
     extra: {
         eas: {
-            projectId: '7b7bb5a4-b819-4974-b898-b41b9ca467a5',
+            projectId: 'b7d1c77f-3535-492c-8f55-0ba569ccdc93',
         },
         EXPO_NODE_ENV: process.env.EXPO_NODE_ENV,
     },
