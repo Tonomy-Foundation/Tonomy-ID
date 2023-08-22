929506e4e0e4064dc1409f10fb0c07d7874a3f2d
Jack Tanner <jack@jackandtheblockstalk.com>
2023-08-22 15:25:36 +0200

    build: setting android::alowBackup false

--- prepare.app.config.ts
+++ prepare.app.config.ts
@@ -59,6 +59,7 @@ const expo: ExpoConfig = {
             foregroundImage: settings.config.images.logo1024,
             backgroundColor: '#FFFFFF',
         },
+        allowBackup: false,
         package: identifier,
     },
     web: {
