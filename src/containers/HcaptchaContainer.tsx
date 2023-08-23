827fbf90b96202e640e2d43bb298ac2c2f657fd4
sadiabbasi <sadiaabbas326@gmail.com>
2023-08-24 03:32:30 +0500

    testing hcaptcha props

--- src/containers/HcaptchaContainer.tsx
+++ src/containers/HcaptchaContainer.tsx
@@ -1,104 +1,75 @@
-import React, { useState, useRef, RefObject, useEffect } from 'react';
-import { Props } from '../screens/HcaptchaScreen';
-import { Text, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
 import ConfirmHcaptcha from '@hcaptcha/react-native-hcaptcha';
-import Hcaptcha from '@hcaptcha/react-native-hcaptcha';
-
 
 const siteKey = '00000000-0000-0000-0000-000000000000';
 const baseUrl = 'https://hcaptcha.com';
 
+interface Props {
+  navigation: any; // Change the type to match your navigation prop type
+}
 
-export default function CreateAccountUsernameContainer({ navigation }: { navigation: Props['navigation'] }) {
-    const [code, setCode] = useState<string | null>(null);
-    const captchaFormRef: RefObject<ConfirmHcaptcha | null> = useRef(null);
-    const hcaptchaRef = React.createRef();
+const CreateAccountUsernameContainer: React.FC<Props> = ({ navigation }) => {
+  const [code, setCode] = useState<string | null>(null);
+  const [showChallenge, setShowChallenge] = useState(true)
+  const captchaFormRef = useRef<ConfirmHcaptcha | null>(null);
 
-    const handleVerify = async () => {
-      if (hcaptchaRef.current) {
-        const result = await hcaptchaRef.current.verify();
-        if (result.success) {
-          // User successfully completed the hCaptcha challenge
-          console.log('hCaptcha verification successful');
-        } else {
-          // User failed or did not complete the hCaptcha challenge
-          console.log('hCaptcha verification failed');
+  useEffect(() => {
+    if (captchaFormRef.current) {
+      captchaFormRef.current.show();
+    }
+  }, []);
+
+  const onMessage = (event: { nativeEvent: { data: string } }) => {
+    if (event && event.nativeEvent.data) {
+      const eventData = event.nativeEvent.data;
+      if (['cancel', 'error', 'expired'].includes(eventData)) {
+        if (captchaFormRef.current) {
+          captchaFormRef.current.hide();
         }
-      }
-    };
-    useEffect(() => {
-        // if (captchaFormRef.current) {
-        //     captchaFormRef.current.show();
-        //   }
-      }, []);
-  
-    const onMessage = (event: { nativeEvent: { data: string } }) => {
-      if (event && event.nativeEvent.data) {
-        const eventData = event.nativeEvent.data;
-        if (['cancel', 'error', 'expired'].includes(eventData)) {
-          if (captchaFormRef.current) {
-            captchaFormRef.current.hide();
-          }
-          setCode(eventData);
-        } else {
-          console.log('Verified code from hCaptcha', eventData);
-          if (captchaFormRef.current) {
-            captchaFormRef.current.hide();
-          }
-          setCode(eventData);
+        setCode(eventData);
+      } else {
+        console.log('Verified code from hCaptcha', eventData);
+        if (captchaFormRef.current) {
+          captchaFormRef.current.hide();
         }
+        setCode(eventData);
       }
-    };
-  
-    return (
-      <View style={styles.container}>
-         {/* <Hcaptcha
-        ref={hcaptchaRef as React.RefObject<Hcaptcha>}
-        siteKey={siteKey}
-        onVerify={handleVerify}
-      />
-      <Button onClick={handleVerify} >Verify</Button> */}
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+         <View style={styles.captchaContainer}>
         <ConfirmHcaptcha
-          size="normal"
-          ref={captchaFormRef as React.RefObject<ConfirmHcaptcha>}
+          size="invisible"
+          ref={captchaFormRef}
           siteKey={siteKey}
           baseUrl={baseUrl}
           languageCode="en"
           onMessage={onMessage}
+          sentry={false}
+          showLoading={false}
+          passiveSiteKey={showChallenge}
         />
-        <TouchableOpacity
-          onPress={() => {
-            if (captchaFormRef.current) {
-              captchaFormRef.current.show();
-            }
-          }}>
-          <Text style={styles.paragraph}>Click to launch</Text>
-        </TouchableOpacity>
-        {code && (
-          <Text style={{ alignSelf: 'center' }}>
-            {`passcode or status: `}
-            <Text style={{ color: 'darkviolet', fontWeight: 'bold', fontSize: 16 }}>
-              {code}
-            </Text>
-          </Text>
-        )}
+        <Button onPress={()=>setShowChallenge(false)} title="Confirm" />
+        {/* Confirm</Button> */}
       </View>
-    );
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-      padding: 8,
-    },
-    paragraph: {
-      margin: 24,
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  });
-  
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  captchaContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default CreateAccountUsernameContainer;
