706c13abc9fc88e95a6aecb4a03fcdd349e18403
sadiabbasi <sadiaabbas326@gmail.com>
2023-08-24 17:45:21 +0300

    revert project id

--- prepare.app.config.ts
+++ prepare.app.config.ts
@@ -75,6 +75,7 @@ const expo: ExpoConfig = {
     ],
     extra: {
         eas: {
+            projectId: '3a65b31c-572d-4537-9ead-f7c4bddf7d29',
         },
         EXPO_NODE_ENV: process.env.EXPO_NODE_ENV,
     },
