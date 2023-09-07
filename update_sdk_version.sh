baedebab0a0dde8d2d8120db0052128a0a31d0ea
Jack Tanner <jack@jackandtheblockstalk.com>
2023-09-07 15:06:53 +0200

    docs: Added comments for upgrade SDK script

--- update_sdk_version.sh
+++ update_sdk_version.sh
@@ -3,13 +3,17 @@
 BRANCH=${1-default}
 CHECK=${2-default}
 
+echo "yarn run updateSdkVersion $@"
+
 set +e
 
+# Get the latest version of the SDK for the correct npmjs tag based on branch
 if [ "${BRANCH}" == "master" ]; then
     VERSION=$(npm view @tonomy/tonomy-id-sdk version)
 elif [ "${BRANCH}" == "development" ]; then
     VERSION=$(npm view @tonomy/tonomy-id-sdk@development version)
 else
+    # Print help
     echo "Usage: yarn run updateSdkVersion master|development [check]"
     echo ""
     echo "Example: yarn run updateSdkVersion development"
@@ -18,11 +22,14 @@ else
     exit 1
 fi
 
+# Get the sha256sum of yarn.lock before updating the SDK
 YARNLOCK_SHA256_OLD=$(sha256sum yarn.lock)
 
+# Update the SDK
 echo "Updating package @tonomy/tonomy-id-sdk@${VERSION}"
 yarn up "@tonomy/tonomy-id-sdk@${VERSION}"
 
+# Get the sha256sum of yarn.lock after updating the SDK
 YARNLOCK_SHA256_NEW=$(sha256sum yarn.lock)
 
 if [ "${CHECK}" == "check" ]; then
