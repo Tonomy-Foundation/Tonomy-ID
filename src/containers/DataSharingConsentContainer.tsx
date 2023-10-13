107af5ad0ef8c214d214c18d140c0e39502fa1e3
Yasin Asadnezhad <49972383+YAS-SIIN@users.noreply.github.com>
2023-10-13 09:25:00 +0330

    Coded Personal Information section.

--- src/containers/DataSharingConsentContainer.tsx
+++ src/containers/DataSharingConsentContainer.tsx
@@ -5,6 +5,10 @@ import { Text, StyleSheet, TouchableOpacity, View, ScrollView, Image } from 'rea
 import { Props } from '../screens/DataSharingConsentScreen';
 import { commonStyles } from '../utils/theme';
 import { TH4 } from '../components/atoms/THeadings';
+import { TButtonText } from '../components/atoms/Tbutton';
+import TList from '../components/TList';
+import { Button, IconButton } from 'react-native-paper';
+import PersonalInformationIcon from '../assets/icons/PersonalInformationIcon';
 
 export default function DataSharingConsentContainer({ navigation }: { navigation: Props['navigation'] }) {
     return (
@@ -13,8 +17,35 @@ export default function DataSharingConsentContainer({ navigation }: { navigation
                 <View style={styles.innerContainer}>
                     <Image width={8} height={8} source={require('../assets/tonomy/tonomy-logo48.png')}></Image>
                 </View>
-                <View style={styles.headerPanel}>
-                    <TH4 style={commonStyles.textAlignCenter}>Jack Tanner</TH4>
+                <View style={styles.boarderPanel}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <IconButton
+                            color={theme.colors.grey2}
+                            style={{ marginLeft: 10 }}
+                            icon="card-account-details"
+                        ></IconButton>
+                        <TH4 style={{ marginLeft: 20, marginTop: 12 }}>Personal Information</TH4>
+                        <IconButton
+                            color={theme.colors.grey2}
+                            onPress={() => alert()}
+                            style={{ position: 'absolute', right: 0, marginLeft: 'auto' }}
+                            icon="clipboard-text-search-outline"
+                        ></IconButton>
+                    </View>
+
+                    <View style={styles.infoListDetail}>
+                        <TList bulletIcon="•" text="Name at Birth" />
+                        <TList bulletIcon="•" text="Current Name" />
+                        <TList bulletIcon="•" text="Last Name" />
+                        <TList bulletIcon="•" text="*Date of Birth" />
+                        <TList bulletIcon="•" text="Gender at Birth" />
+                        <TList bulletIcon="•" text="Current gender" />
+                        <TList bulletIcon="•" text="Address" />
+                        <TList bulletIcon="•" text="Apartment and floor" />
+                        <TList bulletIcon="•" text="Post Code" />
+                        <TList bulletIcon="•" text="City" />
+                        <TList bulletIcon="•" text="Country of Residence" />
+                    </View>
                 </View>
             </ScrollView>
             <Text>test ssss</Text>
