90ad48f1f72766c2154e8401d650b92b5ee4c147
sadiabbasi <sadiaabbas326@gmail.com>
2023-10-23 14:31:52 +0300

    added information profile popup

--- src/containers/ProfilePreviewContainer.tsx
+++ src/containers/ProfilePreviewContainer.tsx
@@ -1,53 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Props } from '../screens/ProfilePreviewScreen';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import TUsername from '../components/TUsername';
 import { TH1, TP } from '../components/atoms/THeadings';
 import theme, { commonStyles } from '../utils/theme';
 import LayoutComponent from '../components/layout';
 import TInfoBox from '../components/TInfoBox';
 import { TButtonContained } from '../components/atoms/Tbutton';
+import TModal from '../components/TModal';
 
 export default function ProfilePreviewContainer({ navigation }: { navigation: Props['navigation'] }) {
+    const [informationPopup, setInformationPopup] = useState(false);
+
     return (
-        <LayoutComponent
-            body={
+        <>
+            <LayoutComponent
+                body={
+                    <View>
+                        <View style={styles.container}>
+                            <TP size={1} style={styles.textColor}>
+                                Username
+                            </TP>
+                            <View>
+                                <TUsername value={'jack.tanner'} disabled={true} />
+                            </View>
+                        </View>
+                        <View style={[styles.container, styles.marginTop]}>
+                            <TP size={1} style={styles.textColor}>
+                                Anonymous User ID
+                                <TouchableOpacity onPress={() => setInformationPopup(true)}>
+                                    <Text style={styles.informationIcon}>?</Text>
+                                </TouchableOpacity>
+                            </TP>
+                            <View>
+                                <TUsername value={'6845 6863 54C6 D346 '} disabled={true} />
+                            </View>
+                        </View>
+                    </View>
+                }
+                footerHint={
+                    <TInfoBox
+                        align="center"
+                        icon="security"
+                        description="Your personal info is self-sovereign meaning only you control who you share it with!"
+                        linkUrl={'#'}
+                        linkUrlText=""
+                    />
+                }
+                footer={
+                    <View style={commonStyles.marginTop}>
+                        <View style={commonStyles.marginBottom}>
+                            <TButtonContained>Back</TButtonContained>
+                        </View>
+                    </View>
+                }
+            />
+            <TModal
+                visible={informationPopup}
+                iconColor={theme.colors.primary}
+                icon="exclamation"
+                title="Anonymus User ID"
+                onPress={() => setInformationPopup(false)}
+            >
                 <View>
-                    <View style={styles.container}>
-                        <TP size={1} style={styles.textColor}>
-                            Username
-                        </TP>
-                        <View>
-                            <TUsername value={'jack.tanner'} disabled={true} />
-                        </View>
-                    </View>
-                    <View style={[styles.container, styles.marginTop]}>
-                        <TP size={1} style={styles.textColor}>
-                            Anonymous User ID <Text style={styles.informationIcon}>?</Text>
-                        </TP>
-                        <View>
-                            <TUsername value={'6845 6863 54C6 D346 '} disabled={true} />
-                        </View>
-                    </View>
+                    <Text>
+                        An Anonymous User ID conceals your personal info, ensuring your transactions and interactions
+                        remain discreet.
+                    </Text>
                 </View>
-            }
-            footerHint={
-                <TInfoBox
-                    align="center"
-                    icon="security"
-                    description="Your personal info is self-sovereign meaning only you control who you share it with!"
-                    linkUrl={'#'}
-                    linkUrlText=""
-                />
-            }
-            footer={
-                <View style={commonStyles.marginTop}>
-                    <View style={commonStyles.marginBottom}>
-                        <TButtonContained>Back</TButtonContained>
-                    </View>
-                </View>
-            }
-        />
+            </TModal>
+        </>
     );
 }
 
