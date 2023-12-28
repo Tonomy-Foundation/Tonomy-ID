be4847e1e2ae4a79fd9c74a89f683ed9ffedeb20
sadiabbasi <sadiaabbas326@gmail.com>
2023-12-28 12:52:38 +0300

    change demo to testnet

--- prepare.app.config.ts
+++ prepare.app.config.ts
@@ -14,9 +14,9 @@ console.log('appInputs', appInputs);
 let slug = settings.config.tonomyIdSlug.replace('://', '');
 const scheme = slug;
 
-if (appInputs.platform === 'ios' && appInputs.expoNodeEnv === 'demo') {
+if (appInputs.platform === 'ios' && appInputs.expoNodeEnv === 'testnet') {
     console.log('Replacing config for demo with some staging config (iOS only');
-    // Deploy staging and demo ios app to the same app store listing
+    // Deploy staging and testnet ios app to the same app store listing
     const config = require('./src/config/config.staging.json');
 
     // Expo slug, and bundleIdentifier must be the same for the same app store listing
