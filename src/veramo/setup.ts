849b3774f6db4a97953b5ab72db10005b94e65c3
sadiabbasi <sadiaabbas326@gmail.com>
2024-05-20 16:36:45 +0300

    added veramo setup

---
+++ src/veramo/setup.ts
@@ -0,0 +1,25 @@
+import { createAgent, IKeyManager } from '@veramo/core';
+import { KeyManager } from '@veramo/key-manager';
+import { KeyManagementSystem, SecretBox } from '@veramo/kms-local';
+import { KeyStore, PrivateKeyStore } from '@veramo/data-store';
+import { DataSource } from 'typeorm';
+
+const DB_ENCRYPTION_KEY = '29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c';
+
+const dbConnection = new DataSource({
+    type: 'expo',
+    driver: require('expo-sqlite'),
+    database: 'veramo.sqlite',
+    logging: ['error', 'info', 'warn'],
+}).initialize();
+
+export const agent = createAgent<IKeyManager>({
+    plugins: [
+        new KeyManager({
+            store: new KeyStore(dbConnection),
+            kms: {
+                local: new KeyManagementSystem(new PrivateKeyStore(dbConnection, new SecretBox(DB_ENCRYPTION_KEY))),
+            },
+        }),
+    ],
+});
