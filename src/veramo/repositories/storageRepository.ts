af71b615a855e0726d2e88d49bdfd5d9a67eebf8
sadiabbasi <sadiaabbas326@gmail.com>
2024-05-23 21:04:47 +0300

    Remove commented codE

--- src/veramo/repositories/storageRepository.ts
+++ src/veramo/repositories/storageRepository.ts
@@ -1,36 +1,3 @@
-// import { Repository, DataSource } from 'typeorm/browser';
-// import { keyStorage } from '../entities/keyStorage';
-
-// export class keyStorageRepository {
-//     private ormRepository: Repository<keyStorage>;
-
-//     constructor(dataSource: DataSource) {
-//         this.ormRepository = dataSource.getRepository(keyStorage);
-//     }
-
-//     public async getAll(): Promise<keyStorage[]> {
-//         return this.ormRepository.find();
-//     }
-
-//     public async create(name: string, value: string): Promise<keyStorage> {
-//         const keyStorage = this.ormRepository.create({ name, value });
-
-//         return this.ormRepository.save(keyStorage);
-//     }
-
-//     public async findByKeyName(name: string): Promise<keyStorage | null> {
-//         return this.ormRepository.findOne({ where: { name } });
-//     }
-
-//     public async update(id: number, value: string): Promise<void> {
-//         await this.ormRepository.update(id, { value });
-//     }
-
-//     public async delete(id: number): Promise<void> {
-//         await this.ormRepository.delete(id);
-//     }
-// }
-
 import { Repository, DataSource } from 'typeorm/browser';
 import { keyStorage } from '../entities/keyStorage';
 import { KeyManager } from './keyManager';
