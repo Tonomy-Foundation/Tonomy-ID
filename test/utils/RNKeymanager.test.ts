2c744ad5cc93558f9b2f9d8714650465a524b921
sadiabbasi <sadiaabbas326@gmail.com>
2024-07-16 16:09:52 +0300

    add test for sign transaction

--- test/utils/RNKeymanager.test.ts
+++ test/utils/RNKeymanager.test.ts
@@ -2,7 +2,9 @@ import { PublicKey } from '@wharfkit/antelope';
 import { KeyManagerLevel } from '@tonomy/tonomy-id-sdk';
 import RNKeyManager from '../../src/utils/RNKeyManager';
 import arg from 'argon2';
-import { generatePrivateKeyFromPassword } from '../../src/utils/keys';
+import { generatePrivateKeyFromPassword, generatePrivateKeyFromSeed } from '../../src/utils/keys';
+import { EthereumAccount, EthereumPrivateKey, EthereumSepoliaChain } from '../../src/utils/chain/etherum';
+import { ethers, TransactionRequest } from 'ethers';
 
 const mockarg = arg;
 
@@ -95,4 +97,31 @@ describe('RN Key Manager', () => {
         expect(key).toBeInstanceOf(PublicKey);
         expect(key.toString()).toEqual(publicKey.toString());
     }, 10000);
+
+    //generate key and sign transaction
+    it('generate private key and sign transaction', async () => {
+        const ethereumKey = await generatePrivateKeyFromSeed('test', EthereumSepoliaChain);
+        const exportPrivateKey = await ethereumKey.exportPrivateKey();
+        const ethereumPrivateKey = new EthereumPrivateKey(exportPrivateKey);
+
+        const ethereumAccount = await EthereumAccount.fromPublicKey(
+            EthereumSepoliaChain,
+            await ethereumPrivateKey.getPublicKey()
+        );
+        const transactionRequest: TransactionRequest = {
+            to: ethereumAccount.getName(),
+            from: ethereumAccount.getName(),
+            value: ethers.parseEther('0'),
+            data: '0x00',
+        };
+
+        const signedTransaction = await ethereumPrivateKey.signTransaction(transactionRequest);
+
+        // Check if signedTransaction is defined and not empty
+        expect(signedTransaction).toBeDefined();
+        expect(signedTransaction).not.toEqual('');
+
+        // Check if signedTransaction is a string in hexadecimal format
+        expect(signedTransaction).toMatch(/^0x[a-fA-F0-9]+$/);
+    }, 30000);
 });
