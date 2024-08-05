fff04cf2cad721561d3c10f6c73f28b95e74ac15
Jack Tanner <jack@jackandtheblockstalk.com>
2024-08-05 11:10:41 +0200

    test: fixed local build system

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
+const Debug = require('debug');
 
+const debug = Debug('tonomy-id:metro.config');
 const config = getDefaultConfig(__dirname);
 
-// needed to resolve pure ESM packages that are within Tonomy-ID-SDK
+// Needed to resolve pure ESM packages that are within Tonomy-ID-SDK
 config.resolver.unstable_enablePackageExports = true;
+
+// Turn on symlinks for local development
+config.resolver.unstable_enableSymlinks = true;
 
 if (process.env.EXPO_NODE_ENV === 'local') {
     console.log('Setting up local development environment. Using local Tonomy-ID-SDK.');
@@ -15,6 +20,12 @@ if (process.env.EXPO_NODE_ENV === 'local') {
 
     config.resolver.nodeModulesPaths.push(sdkPath);
     config.watchFolders.push(sdkPath);
+
+    // Need to resolve these babel-plugin-rewrite-require locally as Tonomy-ID-SDK tries to import them from it's directory
+    config.resolver.extraNodeModules['crypto-browserify'] = path.resolve(__dirname);
+    config.resolver.extraNodeModules['stream-browserify'] = path.resolve(__dirname);
 }
+
+debug('Metro config resolver', config.resolver);
 
 module.exports = config;
