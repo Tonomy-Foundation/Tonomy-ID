92e0b96e9dc1fa76f7bdad307527be502dea519b
sadiabbasi <sadiaabbas326@gmail.com>
2024-08-05 11:29:29 +0300

    added some log

--- test/utils/ethereum.test.ts
+++ test/utils/ethereum.test.ts
@@ -13,27 +13,10 @@ import Web3, { AbiItem } from 'web3';
 import { abi, bytecode } from '../../contracts/SimpleStorage.json';
 
 const mockarg = arg;
-const ganacheUrl = 'http://127.0.0.1:8545';
+const ganacheUrl = 'http://127.0.0.1:7545';
 
 // ABI of the contract
 const contractAbi: AbiItem[] = abi; // Use the actual ABI from the JSON file
-const GanacheChain = new EthereumChain(
-    'http://127.0.0.1:8545',
-    'ganache',
-    '1337',
-    'https://cryptologos.cc/logos/ethereum-eth-logo.png'
-);
-
-const ganacheToken = new EthereumToken(
-    GanacheChain,
-    'Ganache Token',
-    'GNT',
-    18,
-    'https://example.com/path-to-your-token-logo.png',
-    'ganache'
-);
-
-GanacheChain.addToken(ganacheToken);
 
 jest.mock('react-native-argon2', () => {
     return {
@@ -103,6 +86,23 @@ describe('Ethereum sign transaction', () => {
     }, 30000);
 
     it('deploy smart contract and send raw transaction', async () => {
+        const GanacheChain = new EthereumChain(
+            'http://127.0.0.1:7545',
+            'ganache',
+            '1337',
+            'https://cryptologos.cc/logos/ethereum-eth-logo.png'
+        );
+
+        const ganacheToken = new EthereumToken(
+            GanacheChain,
+            'Ganache Token',
+            'GNT',
+            18,
+            'https://example.com/path-to-your-token-logo.png',
+            'ganache'
+        );
+
+        GanacheChain.addToken(ganacheToken);
         // Deploy the contract
         contractInstance = await new web3.eth.Contract(contractAbi)
             .deploy({ data: bytecode })
@@ -128,10 +128,6 @@ describe('Ethereum sign transaction', () => {
         };
 
         console.log('txParams', txParams);
-        const ethereumPrivateKey = new EthereumPrivateKey(senderPrivateKey, GanacheChain);
-
-        console.log('ethereumPrivateKey', await ethereumPrivateKey.getType());
-
         // Increase the timeout for ethers.js provider
         const provider = new ethers.JsonRpcProvider(ganacheUrl);
 
@@ -141,6 +137,10 @@ describe('Ethereum sign transaction', () => {
             console.error('Failed to connect to the network:', error);
             throw error;
         }
+
+        const ethereumPrivateKey = new EthereumPrivateKey(senderPrivateKey, GanacheChain);
+
+        console.log('ethereumPrivateKey', await ethereumPrivateKey.getType());
 
         const transaction = await EthereumTransaction.fromTransaction(ethereumPrivateKey, txParams, GanacheChain);
 
