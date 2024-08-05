a52834fb8410858244f74e1061abc37e86047187
sadiabbasi <sadiaabbas326@gmail.com>
2024-08-05 11:38:13 +0300

    update port

--- test/utils/ethereum.test.ts
+++ test/utils/ethereum.test.ts
@@ -13,10 +13,27 @@ import Web3, { AbiItem } from 'web3';
 import { abi, bytecode } from '../../contracts/SimpleStorage.json';
 
 const mockarg = arg;
-const ganacheUrl = 'http://127.0.0.1:7545';
+const ganacheUrl = 'http://127.0.0.1:8545';
 
 // ABI of the contract
 const contractAbi: AbiItem[] = abi; // Use the actual ABI from the JSON file
+const GanacheChain = new EthereumChain(
+    'http://127.0.0.1:8545',
+    'ganache',
+    '1337',
+    'https://cryptologos.cc/logos/ethereum-eth-logo.png'
+);
+
+const ganacheToken = new EthereumToken(
+    GanacheChain,
+    'Ganache Token',
+    'GNT',
+    18,
+    'https://example.com/path-to-your-token-logo.png',
+    'ganache'
+);
+
+GanacheChain.addToken(ganacheToken);
 
 jest.mock('react-native-argon2', () => {
     return {
@@ -42,6 +59,7 @@ jest.mock('react-native-argon2', () => {
     };
 });
 jest.setTimeout(40000);
+
 describe('Ethereum sign transaction', () => {
     let web3;
     let accounts;
@@ -52,8 +70,12 @@ describe('Ethereum sign transaction', () => {
             web3 = new Web3(ganacheUrl);
             accounts = await web3.eth.getAccounts();
             console.log('Ganache accounts:', accounts);
+
+            if (accounts.length === 0) {
+                throw new Error('No accounts found from Ganache');
+            }
         } catch (error) {
-            console.error('Error connecting to Ganache:', error);
+            console.error('Error connecting to Ganache or fetching accounts:', error);
             throw error; // Fail the test if Ganache is not reachable
         }
     });
@@ -86,23 +108,6 @@ describe('Ethereum sign transaction', () => {
     }, 30000);
 
     it('deploy smart contract and send raw transaction', async () => {
-        const GanacheChain = new EthereumChain(
-            'http://127.0.0.1:7545',
-            'ganache',
-            '1337',
-            'https://cryptologos.cc/logos/ethereum-eth-logo.png'
-        );
-
-        const ganacheToken = new EthereumToken(
-            GanacheChain,
-            'Ganache Token',
-            'GNT',
-            18,
-            'https://example.com/path-to-your-token-logo.png',
-            'ganache'
-        );
-
-        GanacheChain.addToken(ganacheToken);
         // Deploy the contract
         contractInstance = await new web3.eth.Contract(contractAbi)
             .deploy({ data: bytecode })
@@ -128,6 +133,10 @@ describe('Ethereum sign transaction', () => {
         };
 
         console.log('txParams', txParams);
+        const ethereumPrivateKey = new EthereumPrivateKey(senderPrivateKey, GanacheChain);
+
+        console.log('ethereumPrivateKey', await ethereumPrivateKey.getType());
+
         // Increase the timeout for ethers.js provider
         const provider = new ethers.JsonRpcProvider(ganacheUrl);
 
@@ -137,10 +146,6 @@ describe('Ethereum sign transaction', () => {
             console.error('Failed to connect to the network:', error);
             throw error;
         }
-
-        const ethereumPrivateKey = new EthereumPrivateKey(senderPrivateKey, GanacheChain);
-
-        console.log('ethereumPrivateKey', await ethereumPrivateKey.getType());
 
         const transaction = await EthereumTransaction.fromTransaction(ethereumPrivateKey, txParams, GanacheChain);
 
