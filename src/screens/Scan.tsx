20d5d6b237a8266d912fcb221b7887df011c5a6f
Jekin Gohel <jekin1991@gmail.com>
2024-09-03 07:20:47 +0530

    Remove unused files

--- src/screens/Scan.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Scan() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Scan</Text>
-        </View>
-    );
-}
