e50d8ec7fdc18564aaa50955e53c5186f3771f7e
sadiabbasi <sadiaabbas326@gmail.com>
2024-09-06 12:59:43 +0300

    Added debug in initialze

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -82,10 +82,13 @@ const useUserStore = create<UserState>((set, get) => ({
         }
 
         try {
+            debug('initializeStatusFromStorage() try');
             await get().user.initializeFromStorage();
             get().setStatus(UserStatus.LOGGED_IN);
             set({ isAppInitialized: true });
         } catch (e) {
+            debug('initializeStatusFromStorage() error', e);
+
             if (e instanceof SdkError && e.code === SdkErrors.KeyNotFound) {
                 await get().logout('Key not found on account');
                 useErrorStore.getState().setError({ error: e, expected: false });
