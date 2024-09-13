c8aad5438690932e0ff79dc2deda48ebe7a0044e
sadiabbasi <sadiaabbas326@gmail.com>
2024-09-13 22:06:27 +0300

    frezzing resolve when comment out communication service in navigation uncomment connected account

--- src/services/CommunicationModule.ts
+++ src/services/CommunicationModule.ts
@@ -50,14 +50,20 @@ export default function CommunicationModule() {
             try {
                 await user.loginCommunication(message);
             } catch (e) {
+                if (e.message === 'Network request failed') {
+                    debug('Network error in communication login');
+                }
                 // 401 signature invalid: the keys have been rotated and the old key is no longer valid
                 // 404 did not found: must have changed network (blockchain full reset - should only happen on local dev)
-                if (e instanceof CommunicationError && (e.exception.status === 401 || e.exception.status === 404)) {
+                else if (
+                    e instanceof CommunicationError &&
+                    (e.exception.status === 401 || e.exception.status === 404)
+                ) {
                     await logout(
                         e.exception.status === 401 ? 'Communication key rotation' : 'Communication key not found'
                     );
                 } else {
-                    throw e;
+                    errorStore.setError({ error: e, expected: false });
                 }
             }
         } catch (e) {
@@ -92,7 +98,11 @@ export default function CommunicationModule() {
                 });
                 sendLoginNotificationOnBackground(payload.requests[0].getPayload().origin);
             } catch (e) {
-                errorStore.setError({ error: e, expected: false });
+                if (e.message === 'Network request failed') {
+                    debug('Network error in communication login');
+                } else {
+                    errorStore.setError({ error: e, expected: false });
+                }
             }
         }, LoginRequestsMessage.getType());
 
@@ -109,7 +119,11 @@ export default function CommunicationModule() {
 
                 await user.handleLinkAuthRequestMessage(message);
             } catch (e) {
-                errorStore.setError({ error: e, expected: false });
+                if (e.message === 'Network request failed') {
+                    debug('Network error in communication login');
+                } else {
+                    errorStore.setError({ error: e, expected: false });
+                }
             }
         }, LinkAuthRequestMessage.getType());
 
@@ -154,165 +168,173 @@ export default function CommunicationModule() {
     }
 
     const handleConnect = useCallback(async () => {
-        const onSessionProposal = async (proposal) => {
-            try {
-                const { id } = proposal;
-                const { requiredNamespaces, optionalNamespaces } = proposal.params;
-                const activeNamespaces = Object.keys(requiredNamespaces).length
-                    ? requiredNamespaces
-                    : optionalNamespaces;
-                const chainIds = activeNamespaces.eip155.chains?.map((chain) => chain.split(':')[1]);
-                const unsupportedChainIds =
-                    chainIds?.filter((chainId) => !['1', '11155111', '137'].includes(chainId)) || [];
+        try {
+            const onSessionProposal = async (proposal) => {
+                try {
+                    const { id } = proposal;
+                    const { requiredNamespaces, optionalNamespaces } = proposal.params;
+                    const activeNamespaces = Object.keys(requiredNamespaces).length
+                        ? requiredNamespaces
+                        : optionalNamespaces;
+                    const chainIds = activeNamespaces.eip155.chains?.map((chain) => chain.split(':')[1]);
+                    const unsupportedChainIds =
+                        chainIds?.filter((chainId) => !['1', '11155111', '137'].includes(chainId)) || [];
 
-                if (unsupportedChainIds?.length > 0) {
-                    errorStore.setError({
-                        title: 'Unsupported Chains',
-                        error: new Error(
-                            'We currently support Ethereum Mainnet, Sepolia Testnet, and Polygon Mainnet.'
-                        ),
-                        expected: true,
-                    });
-                    await web3wallet?.rejectSession({
-                        id: id,
-                        reason: getSdkError('UNSUPPORTED_CHAINS'),
-                    });
-                    return;
-                } else {
-                    const supportedChains = {
-                        '1': { name: 'ethereum', chainObject: EthereumMainnetChain },
-                        '11155111': { name: 'ethereumTestnetSepolia', chainObject: EthereumSepoliaChain },
-                        '137': { name: 'ethereumPolygon', chainObject: EthereumPolygonChain },
-                    };
+                    if (unsupportedChainIds?.length > 0) {
+                        errorStore.setError({
+                            title: 'Unsupported Chains',
+                            error: new Error(
+                                'We currently support Ethereum Mainnet, Sepolia Testnet, and Polygon Mainnet.'
+                            ),
+                            expected: true,
+                        });
+                        await web3wallet?.rejectSession({
+                            id: id,
+                            reason: getSdkError('UNSUPPORTED_CHAINS'),
+                        });
+                        return;
+                    } else {
+                        const supportedChains = {
+                            '1': { name: 'ethereum', chainObject: EthereumMainnetChain },
+                            '11155111': { name: 'ethereumTestnetSepolia', chainObject: EthereumSepoliaChain },
+                            '137': { name: 'ethereumPolygon', chainObject: EthereumPolygonChain },
+                        };
 
-                    let keyFound = false;
+                        let keyFound = false;
 
-                    for (const chainId of chainIds) {
-                        if (supportedChains[chainId]) {
-                            const { name, chainObject } = supportedChains[chainId];
-                            const key = await keyStorage.findByName(name, chainObject);
+                        for (const chainId of chainIds) {
+                            if (supportedChains[chainId]) {
+                                const { name, chainObject } = supportedChains[chainId];
+                                const key = await keyStorage.findByName(name, chainObject);
 
-                            if (!key) {
-                                navigation.navigate('CreateEthereumKey', {
-                                    requestType: 'loginRequest',
-                                    payload: proposal,
-                                });
-                                return;
-                            } else {
-                                keyFound = true;
+                                if (!key) {
+                                    navigation.navigate('CreateEthereumKey', {
+                                        requestType: 'loginRequest',
+                                        payload: proposal,
+                                    });
+                                    return;
+                                } else {
+                                    keyFound = true;
+                                }
                             }
                         }
-                    }
 
-                    if (keyFound) {
-                        navigation.navigate('WalletConnectLogin', {
-                            payload: proposal,
-                            platform: 'browser',
-                        });
-                    }
-                }
-            } catch (error) {
-                if (error.message === 'Network request failed') {
-                    debug('network error when initializing wallet account');
-                }
-            }
-        };
-
-        const onSessionRequest = async (event) => {
-            try {
-                const { topic, params, id, verifyContext } = event;
-                const { request, chainId } = params;
-
-                switch (request.method) {
-                    case 'eth_sendTransaction': {
-                        const transactionData = request.params[0];
-
-                        let key, chain;
-
-                        if (chainId === 'eip155:11155111') {
-                            chain = EthereumSepoliaChain;
-                            key = await keyStorage.findByName('ethereumTestnetSepolia', chain);
-                        } else if (chainId === 'eip155:1') {
-                            chain = EthereumMainnetChain;
-                            key = await keyStorage.findByName('ethereum', chain);
-                        } else if (chainId === 'eip155:137') {
-                            chain = EthereumPolygonChain;
-                            key = await keyStorage.findByName('ethereumPolygon', chain);
-                        } else throw new Error('Unsupported chains');
-
-                        let transaction: ITransaction;
-
-                        if (key) {
-                            const exportPrivateKey = await key.exportPrivateKey();
-                            const ethereumPrivateKey = new EthereumPrivateKey(exportPrivateKey, chain);
-
-                            transaction = await EthereumTransaction.fromTransaction(
-                                ethereumPrivateKey,
-                                transactionData,
-                                chain
-                            );
-                            navigation.navigate('SignTransaction', {
-                                transaction,
-                                privateKey: key,
-                                session: {
-                                    origin: verifyContext?.verified?.origin,
-                                    id,
-                                    topic,
-                                },
+                        if (keyFound) {
+                            navigation.navigate('WalletConnectLogin', {
+                                payload: proposal,
+                                platform: 'browser',
                             });
-                        } else {
-                            transaction = new EthereumTransaction(transactionData, chain);
-                            navigation.navigate('CreateEthereumKey', {
-                                requestType: 'transactionRequest',
-                                transaction: {
+                        }
+                    }
+                } catch (error) {
+                    if (error.message === 'Network request failed') {
+                        debug('network error when initializing wallet account');
+                    }
+                }
+            };
+
+            const onSessionRequest = async (event) => {
+                try {
+                    const { topic, params, id, verifyContext } = event;
+                    const { request, chainId } = params;
+
+                    switch (request.method) {
+                        case 'eth_sendTransaction': {
+                            const transactionData = request.params[0];
+
+                            let key, chain;
+
+                            if (chainId === 'eip155:11155111') {
+                                chain = EthereumSepoliaChain;
+                                key = await keyStorage.findByName('ethereumTestnetSepolia', chain);
+                            } else if (chainId === 'eip155:1') {
+                                chain = EthereumMainnetChain;
+                                key = await keyStorage.findByName('ethereum', chain);
+                            } else if (chainId === 'eip155:137') {
+                                chain = EthereumPolygonChain;
+                                key = await keyStorage.findByName('ethereumPolygon', chain);
+                            } else throw new Error('Unsupported chains');
+
+                            let transaction: ITransaction;
+
+                            if (key) {
+                                const exportPrivateKey = await key.exportPrivateKey();
+                                const ethereumPrivateKey = new EthereumPrivateKey(exportPrivateKey, chain);
+
+                                transaction = await EthereumTransaction.fromTransaction(
+                                    ethereumPrivateKey,
+                                    transactionData,
+                                    chain
+                                );
+                                navigation.navigate('SignTransaction', {
                                     transaction,
+                                    privateKey: key,
                                     session: {
                                         origin: verifyContext?.verified?.origin,
                                         id,
                                         topic,
                                     },
-                                },
-                            });
+                                });
+                            } else {
+                                transaction = new EthereumTransaction(transactionData, chain);
+                                navigation.navigate('CreateEthereumKey', {
+                                    requestType: 'transactionRequest',
+                                    transaction: {
+                                        transaction,
+                                        session: {
+                                            origin: verifyContext?.verified?.origin,
+                                            id,
+                                            topic,
+                                        },
+                                    },
+                                });
+                            }
+
+                            sendWalletConnectNotificationOnBackground(
+                                'Transaction Request',
+                                'Ethereum transaction signing request'
+                            );
+                            break;
                         }
 
-                        sendWalletConnectNotificationOnBackground(
-                            'Transaction Request',
-                            'Ethereum transaction signing request'
-                        );
-                        break;
+                        default: {
+                            const response = {
+                                id: id,
+                                error: getSdkError('UNSUPPORTED_METHODS'),
+                                jsonrpc: '2.0',
+                            };
+
+                            await web3wallet?.respondSessionRequest({
+                                topic,
+                                response,
+                            });
+                            return;
+                        }
                     }
-
-                    default: {
-                        const response = {
-                            id: id,
-                            error: getSdkError('UNSUPPORTED_METHODS'),
-                            jsonrpc: '2.0',
-                        };
-
-                        await web3wallet?.respondSessionRequest({
-                            topic,
-                            response,
-                        });
-                        return;
+                } catch (error) {
+                    if (error.message === 'Network request failed') {
+                        debug('network error when initializing wallet account');
                     }
                 }
-            } catch (error) {
-                if (error.message === 'Network request failed') {
-                    debug('network error when initializing wallet account');
-                }
-            }
-        };
+            };
 
-        web3wallet?.off('session_proposal', onSessionProposal);
-        web3wallet?.off('session_request', onSessionRequest);
-
-        web3wallet?.on('session_proposal', onSessionProposal);
-        web3wallet?.on('session_request', onSessionRequest);
-
-        return () => {
             web3wallet?.off('session_proposal', onSessionProposal);
             web3wallet?.off('session_request', onSessionRequest);
-        };
+
+            web3wallet?.on('session_proposal', onSessionProposal);
+            web3wallet?.on('session_request', onSessionRequest);
+
+            return () => {
+                web3wallet?.off('session_proposal', onSessionProposal);
+                web3wallet?.off('session_request', onSessionRequest);
+            };
+        } catch (e) {
+            if (e.message === 'Network request failed') {
+                debug('network error when listening wallet requests');
+            } else {
+                debug('handle session proposal error', e);
+            }
+        }
     }, [navigation, web3wallet, errorStore]);
 
     useEffect(() => {
@@ -329,33 +351,41 @@ export default function CommunicationModule() {
     };
 
     useEffect(() => {
-        const handleSessionDelete = debounce(async (event) => {
-            try {
-                if (event.topic) {
-                    const sessions = await web3wallet?.getActiveSessions();
-                    const sessionExists =
-                        Array.isArray(sessions) && sessions.some((session) => session.topic === event.topic);
+        try {
+            const handleSessionDelete = debounce(async (event) => {
+                try {
+                    if (event.topic) {
+                        const sessions = await web3wallet?.getActiveSessions();
+                        const sessionExists =
+                            Array.isArray(sessions) && sessions.some((session) => session.topic === event.topic);
 
-                    if (sessionExists) {
-                        await web3wallet?.disconnectSession({
-                            topic: event.topic,
-                            reason: getSdkError('INVALID_SESSION_SETTLE_REQUEST'),
-                        });
-                        disconnectSession();
-                    } else {
-                        debug('Session already deleted or invalid');
+                        if (sessionExists) {
+                            await web3wallet?.disconnectSession({
+                                topic: event.topic,
+                                reason: getSdkError('INVALID_SESSION_SETTLE_REQUEST'),
+                            });
+                            disconnectSession();
+                        } else {
+                            debug('Session already deleted or invalid');
+                        }
                     }
+                } catch (disconnectError) {
+                    console.error('Failed to disconnect session:', disconnectError);
                 }
-            } catch (disconnectError) {
-                console.error('Failed to disconnect session:', disconnectError);
+            }, 1000);
+
+            web3wallet?.on('session_delete', handleSessionDelete);
+
+            return () => {
+                web3wallet?.off('session_delete', handleSessionDelete);
+            };
+        } catch (e) {
+            if (e.message === 'Network request failed') {
+                debug('network error when listening wallet requests');
+            } else {
+                debug('handle session delete error', e);
             }
-        }, 1000);
-
-        web3wallet?.on('session_delete', handleSessionDelete);
-
-        return () => {
-            web3wallet?.off('session_delete', handleSessionDelete);
-        };
+        }
     }, [web3wallet, disconnectSession, navigation, errorStore]);
 
     return null;
