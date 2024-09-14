a5df7d81aeded4fd1ac3bbda0b242ad0f31a07ef
sadiabbasi <sadiaabbas326@gmail.com>
2024-09-14 19:10:58 +0300

    comment out wallet code from splash

--- src/containers/MainSplashContainer.tsx
+++ src/containers/MainSplashContainer.tsx
@@ -19,7 +19,7 @@ const debug = Debug('tonomy-id:container:mainSplashScreen');
 export default function MainSplashScreenContainer({ navigation }: { navigation: Props['navigation'] }) {
     const errorStore = useErrorStore();
     const { user, initializeStatusFromStorage, isAppInitialized, getStatus, logout, setStatus } = useUserStore();
-    const { clearState, initializeWalletState, initializeWalletAccount, initialized, accountExists } = useWalletStore();
+    // const { clearState, initializeWalletState, initializeWalletAccount, initialized, accountExists } = useWalletStore();
 
     useEffect(() => {
         async function main() {
@@ -55,27 +55,27 @@ export default function MainSplashScreenContainer({ navigation }: { navigation: 
                         try {
                             await user.getUsername();
 
-                            if (!initialized) {
-                                try {
-                                    progressiveRetryOnNetworkError(async () => await initializeWalletState());
-                                } catch (e) {
-                                    console.error('Error initializing wallet:', e);
-                                    errorStore.setError({ error: e, expected: false });
-                                }
-                            }
+                            // if (!initialized) {
+                            //     try {
+                            //         progressiveRetryOnNetworkError(async () => await initializeWalletState());
+                            //     } catch (e) {
+                            //         console.error('Error initializing wallet:', e);
+                            //         errorStore.setError({ error: e, expected: false });
+                            //     }
+                            // }
 
-                            if (!accountExists) {
-                                try {
-                                    progressiveRetryOnNetworkError(async () => await initializeWalletAccount());
-                                } catch (e) {
-                                    console.error('Error initializing wallet accounts:', e);
-                                    errorStore.setError({ error: e, expected: false });
-                                }
-                            }
+                            // if (!accountExists) {
+                            //     try {
+                            //         progressiveRetryOnNetworkError(async () => await initializeWalletAccount());
+                            //     } catch (e) {
+                            //         console.error('Error initializing wallet accounts:', e);
+                            //         errorStore.setError({ error: e, expected: false });
+                            //     }
+                            // }
                         } catch (e) {
                             if (e instanceof SdkError && e.code === SdkErrors.InvalidData) {
                                 logout("Invalid data in user's storage");
-                                clearState();
+                                // clearState();
                             } else {
                                 debug('loggedin error', e);
                                 throw e;
@@ -105,13 +105,13 @@ export default function MainSplashScreenContainer({ navigation }: { navigation: 
         logout,
         navigation,
         user,
-        initializeWalletState,
-        clearState,
+        // initializeWalletState,
+        // clearState,
         setStatus,
         isAppInitialized,
-        initializeWalletAccount,
-        initialized,
-        accountExists,
+        // initializeWalletAccount,
+        // initialized,
+        // accountExists,
     ]);
 
     return (
