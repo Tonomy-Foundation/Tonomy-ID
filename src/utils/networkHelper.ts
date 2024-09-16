63bba06bd79732c96380a67bb939c11fdc789c89
sadiabbasi <sadiaabbas326@gmail.com>
2024-09-16 21:32:44 +0300

    remove unused import fix typeCheck

--- src/utils/networkHelper.ts
+++ src/utils/networkHelper.ts
@@ -1,11 +1,10 @@
 import NetInfo from '@react-native-community/netinfo';
 import { useEffect, useState } from 'react';
 import useWalletStore from '../store/useWalletStore';
-import { ICore } from '@walletconnect';
 
 const useNetworkStatus = () => {
     const [isConnected, setIsConnected] = useState<boolean | null>(null);
-    const { disconnectSession, core } = useWalletStore();
+    const { core } = useWalletStore();
 
     useEffect(() => {
         // Subscribe to network state updates
