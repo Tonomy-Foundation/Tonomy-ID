d2b5a46e079753bb77a79b4eee5754e141350fd1
sadiabbasi <sadiaabbas326@gmail.com>
2024-09-17 19:16:51 +0300

    revert else status case

--- src/containers/MainSplashContainer.tsx
+++ src/containers/MainSplashContainer.tsx
@@ -87,7 +87,7 @@ export default function MainSplashScreenContainer({ navigation }: { navigation: 
                 } else {
                     debug('main screen error', e);
                     errorStore.setError({ error: e, expected: false });
-                    navigation.dispatch(StackActions.replace('Home'));
+                    navigation.navigate('SplashSecurity');
                 }
             }
         }
