8cb0bb75401be2cbeb48e9b01174f9c827e51e1f
sadiabbasi <sadiaabbas326@gmail.com>
2024-10-28 12:55:08 +0300

    fixed the antelope push transaction error not expandable

--- src/utils/chain/antelope.ts
+++ src/utils/chain/antelope.ts
@@ -44,6 +44,7 @@ import { IdentityV3, ResolvedSigningRequest } from '@wharfkit/signing-request';
 import Debug from 'debug';
 import { createUrl, getQueryParam } from '../strings';
 import { ApplicationErrors, throwError } from '../errors';
+import { AntelopePushTransactionError, HttpError } from '@tonomy/tonomy-id-sdk';
 
 const debug = Debug('tonomy-id:utils:chain:antelope');
 
@@ -194,6 +195,14 @@ export class AntelopePrivateKey extends AbstractPrivateKey implements IPrivateKe
                 )
             ) {
                 throwError('Incorrect Transaction Authorization', ApplicationErrors.IncorrectTransactionAuthorization);
+            }
+
+            if (error.response?.headers) {
+                if (error.response?.json) {
+                    throw new AntelopePushTransactionError({ ...error.response.json });
+                }
+
+                throw new HttpError(error);
             }
 
             throw error;
