af8ac55f1fafbfb724253f0f0f569c5a2a276e6c
sadiabbasi <sadiaabbas326@gmail.com>
2024-11-04 23:36:21 +0300

    remove unused session helper

--- src/utils/session/helper.ts
+++
@@ -1,8 +0,0 @@
-import { supportedChains } from '../assetDetails';
-import { EthereumAccount } from '../chain/etherum';
-
-export const getChainIds = (namespaces: any) =>
-    namespaces.eip155?.chains?.map((chain: string) => chain.split(':')[1]) || [];
-
-export const hasUnsupportedChains = (chainIds: string[]) =>
-    chainIds.some((chainId) => !['1', '11155111', '137'].includes(chainId));
