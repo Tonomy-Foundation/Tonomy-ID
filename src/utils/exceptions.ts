6244554574bbc3607e6e5e1cc4f68a9d22b6acb6
sadiabbasi <sadiaabbas326@gmail.com>
2024-12-03 00:17:29 +0300

    change the debug to console.error in exceptions

--- src/utils/exceptions.ts
+++ src/utils/exceptions.ts
@@ -18,7 +18,7 @@ export default function setErrorHandlers(errorStore: ErrorState) {
         if (isFatal) {
             errorStore.setError({ error: e, title: 'Unexpected Fatal JS Error', expected: false });
         } else {
-            debug('Unexpected JS Error Logs', e, typeof e, JSON.stringify(e, null, 2));
+            console.error('Unexpected JS Error Logs', e, typeof e, JSON.stringify(e, null, 2));
 
             // @ts-expect-error context does not exist on Error
             if (e?.context?.startsWith('core') && e?.time && e?.level) {
