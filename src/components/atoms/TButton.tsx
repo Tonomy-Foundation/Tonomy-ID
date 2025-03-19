2c6ec505357025cf7315f4d9c4f9ab709dac4b29
sadiabbasi <sadiaabbas326@gmail.com>
2025-03-20 00:29:48 +0300

    fix: :zap: revert vertical padding from tbutton

--- src/components/atoms/TButton.tsx
+++ src/components/atoms/TButton.tsx
@@ -62,10 +62,10 @@ export function TButtonContained(props: ButtonProps) {
     const color = theme.colors.white;
     const style: ViewStyle = {
         backgroundColor: props.disabled ? theme.colors.disabled : getColorBasedOnTheme(props.theme),
+        height: 48,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
-        paddingVertical: props.size !== 'large' ? 11 : 14,
     };
     const shadowStyle = {
         shadowColor: theme.colors.shadowDark,
