c14ff20b099bd760c66b7e3897dc2d21da0b794a
sadiabbasi <sadiaabbas326@gmail.com>
2025-08-07 18:23:18 +0300

    fix: :zap: fixed back icon on sso

--- src/navigation/Root.tsx
+++ src/navigation/Root.tsx
@@ -242,7 +242,7 @@ export default function RootNavigation() {
                         />
                         <Stack.Screen
                             name="SSO"
-                            options={{ ...noHeaderScreenOptions, title: 'App login' }}
+                            options={{ headerBackTitleVisible: false, headerBackVisible: false, title: 'App login' }}
                             component={SSOLoginScreen}
                         />
                         <Stack.Screen
@@ -364,7 +364,7 @@ export default function RootNavigation() {
                         />
                         <Stack.Screen
                             name="VeriffDataSharing"
-                            options={{ ...noHeaderScreenOptions, title: 'Data Sharing' }}
+                            options={{ headerBackTitleVisible: false, headerBackVisible: false, title: 'Data Sharing' }}
                             component={VeriffDataSharingScreen}
                         />
                         <Stack.Screen
